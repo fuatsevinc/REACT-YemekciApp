16d31751fed7e6a9f66d6eaaa4fbe22ea323e2ad
fuatsevinc <fuatsevinc@icloude.com>
2022-04-10 18:02:01 +0200

    navbar added

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Logo, Nav, Hamburger, Menu, MenuLink } from "./NavbarStyles";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Nav>
+      <Logo to="/home">
+        <i>{"<Clarusway>"}</i>
+        <span>recipe</span>
+      </Logo>
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+        <span />
+        <span />
+        <span />
+      </Hamburger>
+
+      <Menu osman={isOpen}>
+        <MenuLink to="/about">About</MenuLink>
+        <MenuLink to="/github">Github</MenuLink>
+        <MenuLink to="/">Logout</MenuLink>
+      </Menu>
+    </Nav>
+  );
+};
+
+export default Navbar;
